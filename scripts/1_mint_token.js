0e1232dcfe16c5bfb8d41c28f9b9b9bba58434da
Maxime GRIS <maxime.gris@gmail.com>
2024-10-05 17:25:21 +0200

    fetch NFTs from simplehash

--- scripts/1_mint_token.js
+++ scripts/1_mint_token.js
@@ -10,8 +10,10 @@ async function main() {
     const contractAddress = '0x8De08A2CE86F838416c509499802c906F492D030'; /* TODO: Address of the contract */
     const contract = new ethers.Contract(contractAddress, Example.abi, provider);
 
-    const txSetTokenURI = await contract.connect(owner).safeMint('ipfs://bafybeibulyuw4qmptj3z4kujjh2w5677xx3eizwydz2yao3nnrt7fhsnlq/1000.json', '0x0b1De2495d78C65eC7e0C3978BC625b019360F11');
+    const txSetTokenURI = await contract.connect(owner).safeMint('ipfs://bafybeibulyuw4qmptj3z4kujjh2w5677xx3eizwydz2yao3nnrt7fhsnlq/1000.json', '0x4F1f538cd13FEbcB36Df69D763eA4EADEb1FCDb0');
     await txSetTokenURI.wait();
+
+    console.log('NFT minted and tokenURI set');
 }
 
 main().catch((error) => {
--- server/src/api/controllers/myMooguis.js
+++ server/src/api/controllers/myMooguis.js
@@ -1,4 +1,6 @@
 const {Request, Response} = require('express');
+const fs = require('fs');
+const axios = require('axios');
 
 /**
  * User MooguisAPI.
@@ -9,9 +11,45 @@ const {Request, Response} = require('express');
  */
 const myMooguis = async (req, res) => {
     try {
-        // Fetch Mooguis NFT on chain. Use a API like SimpleHash to get the NFTs.
-        // (can fetch NFTs on a specific Smart Contract Address & Wallet)
-        return res.status(200).json({message: 'Récupération liste mooguis', data: []});
+        fs.readFile('./src/resources/comptes.txt', 'utf8', async (err, data) => {
+            if (err) {
+                return res.status(500).json({message: 'Erreur de lecture des comptes.'});
+            }
+
+            const users = data.split('\n');
+            const user = users.find(u => u.startsWith(`${req.user.email}`));
+
+            if (user) {
+                // user = email###password###walletAddress###privateKey
+                const values = user.split('###');
+                const obj = {
+                    email: values[0],
+                    password: values[1],
+                    walletAddress: values[2],
+                    privateKey: values[3]
+                };
+
+                const response = await axios.get('https://api.simplehash.com/api/v0/nfts/owners',       {
+                    headers: {
+                        'Accept': 'application/json',
+                        'X-API-KEY': process.env.SIMPLE_HASH_SECRET
+                    },
+                    params: {
+                        wallet_addresses: obj.walletAddress,
+                        chains: 'avalanche-fuji',
+                        contract_addresses: process.env.NFT_CONTRACT_ADDRESS
+                    }
+                });
+
+                if(response.data?.nfts) {
+                    return res.status(200).json({message: 'Récupération liste mooguis', data: response.data.nfts});
+                } else {
+                    return res.status(200).json({message: 'Récupération liste mooguis', data: []});
+                }
+            } else {
+                return res.status(401).json({message: 'Email ou mot de passe incorrect.'});
+            }
+        });
     } catch (e) {
         res.status(400).send(JSON.stringify(e));
     }
